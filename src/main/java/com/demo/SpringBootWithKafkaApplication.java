6a8e65a991be6e1582b255c07b3d04a127a4107b
Michael McCaskill <michael.mccaskill@cengage.com>
2021-05-11 20:01:30 -0400

    Switch to keystore-utils 0.0.3
    This version uses a `CfEnvProcessor` to read from CredHub the
    key/trustore and put them into the `spring.kafka` properties

--- src/main/java/com/demo/SpringBootWithKafkaApplication.java
+++ src/main/java/com/demo/SpringBootWithKafkaApplication.java
@@ -2,13 +2,11 @@ package com.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import sh.buildit.credhub.KeyStoreUtils;
 
 @SpringBootApplication
 public class SpringBootWithKafkaApplication {
 
 	public static void main(String[] args) {
-		KeyStoreUtils.initialize();
 		SpringApplication.run(SpringBootWithKafkaApplication.class, args);
 	}
 
